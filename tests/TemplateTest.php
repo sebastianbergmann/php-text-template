82be9cbd4dca8d8ee1c98f252ed7e52b58c5e87c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-01-15 10:44:58 +0100

    Use attributes instead of annotations

--- tests/TemplateTest.php
+++ tests/TemplateTest.php
@@ -9,11 +9,12 @@
  */
 namespace SebastianBergmann\Template;
 
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\TestCase;
 
-/**
- * @covers \SebastianBergmann\Template\Template
- */
+#[CoversClass(Template::class)]
+#[Small]
 final class TemplateTest extends TestCase
 {
     public function testRendersFromGivenTemplateFileToString(): void
